064dcc62aca22ce2d4f49e7c233a5039dd859e48
AdityaANH <aditya.n.h02@gmail.com>
2022-03-25 18:00:26 +0700

    menambahkan class SayaTubeVideo

---
+++ tpmodul5_1302204078/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302204078
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo a = new SayaTubeVideo("Tutorial Design By Contract – [Aditya Nur Hidayatullah]");
+            a.PrintVideoDetails();
+            a.IncreasePlayCount(1);
+            a.PrintVideoDetails();
+        }
+    }
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string a)
+        {
+            // Instantiate random number generator using system-supplied value as seed.
+            Random b = new Random();
+
+            // Generate and display 5 random integers.
+            Console.WriteLine("Five random integer values:");
+            for (int ctr = 0; ctr <= 4; ctr++)
+                Console.Write("{0,15:N0}", b.Next());
+            Console.WriteLine();
+
+
+            this.title = a;
+            playCount = 0;
+            setPlayCount(123);
+        }
+        public void IncreasePlayCount(int a)
+        {
+            playCount += a;
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("id :" + id);
+            Console.WriteLine("Title :" + title);
+            Console.WriteLine("PlayCount : " + playCount);
+            Console.WriteLine();
+        }
+        public void setPlayCount(int a)
+        {
+            this.playCount = a;
+        }
+        public string getTitle()
+        {
+            return this.title;
+        }
+    }
+}
